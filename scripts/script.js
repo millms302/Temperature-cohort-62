cfd3871bdfd9f20b59832ab75d5425a4dae9a424
Mike Miller <millms302@gmail.com>
2025-09-27 14:04:44 -0500

    practice

---
+++ scripts/script.js
@@ -0,0 +1,52 @@
+console.log("Hello World from JS");
+
+//double slashes are for comments on a single line
+/*
+*this is a multiple line comment
+*And I can be used to describe a function or even a file
+*/
+
+//console.log("This is a comment")
+
+//Variables and Constants
+//keyboard nameVar=value
+//var, let and const
+
+//Data Types
+//Strings (text), numerical and Boolean
+//Strings are defined with '' or "" (single or double quotes)
+var firstName = "Mike"; //String
+let lastName = "Miller"; //String
+
+console.log (firstName);
+console.log (lastName);
+
+//numbers
+let myAge = 24;
+let myHeight = 6.16;
+let myAgeTwo = "24";
+
+console.log (myAge);
+console.log (myHeight);
+console.log (myAgeTwo);
+
+//Boolean - True or False
+let isStudent = false;
+
+//Display Values in Console
+//Concatena
+console.log("My name is: "+ firstName + " " + lastName + " " + "years old, my size is: " + myHeight);
+
+//Arithmatic Operations: + - / *
+let num1 = 10;
+let num2 = 2;
+
+let sum = num1 + num2; //Addition -> +
+let sub = num1 - num2; //Subtravtion -> -
+let mul = num1 * num2; //Multiplication -> *
+let div = num1 / num2; //Division -> /
+
+console.log("The sum is:"+ sum)//12
+console.log("The sub is:"+ sub)//8
+console.log("the mul is:"+ mul)//20
+console.log("the div is:"+div)//5
