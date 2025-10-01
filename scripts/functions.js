7b92668e324035acef555dbd5cf6e9ce555627b9
Mike Miller <millms302@gmail.com>
2025-09-30 21:55:30 -0500

    FunctionPract

--- scripts/functions.js
+++ scripts/functions.js
@@ -46,5 +46,57 @@ function Welcome(){
     let name = prompt(" Enter Your Name:");
     console.log("Welcome " + name);
 }
+//trigger from the console
 
-Welcome()
+//Example #5 - with default parameters
+
+function addNumbers(num1=0, num2=0){
+    let total = num1 + num2;
+    console.log(`The total is: ${total}`)
+}
+addNumbers(2,5);
+addNumbers(5);
+addNumbers();
+
+// Challenge 2: Combine Names
+// Task:
+// - Create a function called 'combineNames' that takes two parameters: `firstName` and `lastName'.
+// - The function should return the full names in the format: `"firstName lastName"
+
+function combineNames(firstName="unknown", lastName="unknown"){
+    console.log(`${firstName} ${lastName}`)
+}
+
+combineNames("Mike", "Miller"); // Mike Miller
+combineNames("Bill", "Buttlicker") // Bill Buttlicker
+
+// Challenge 3: convert minutes to seconds
+// - Create a funtion called 'convertToSeconds' that takes one parameter, 'minutes'.
+// - The function should retturn the number of seconds by multiplying the minutes by 60.
+// - call the fuinction with different values of minutes and print the results.
+
+
+// My example
+function convertToSeconds(minutes){
+    let total2 = minutes * (60)
+    console.log(`The total number of seconds is: ${total2}`)
+}
+convertToSeconds(55) // 55 Minutes
+convertToSeconds(6874346) // 6,874,346 Minutes
+
+
+// Doc Jiminez example
+function convertToSeconds(mins){
+    let sec = mins*60;
+    console.log(`${mins} min are ${sec}s`);
+}
+convertToSeconds(2)
+
+//convert to seconds way 3:
+
+function convertToSeconds(){
+    let mins = Number(prompt("Enter the Min"));
+    let sec = mins*60;
+
+    document.getElementById("results").innerHTML=`<p>${sec}<p>`;
+}
