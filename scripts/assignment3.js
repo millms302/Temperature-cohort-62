1319f65789a4fe4f85ed5226b7ac608ed4578da9
Mike Miller <millms302@gmail.com>
2025-10-06 18:14:49 -0500

    assignment3final

--- scripts/assignment3.js
+++ scripts/assignment3.js
@@ -1,8 +1,15 @@
 function convertTemperature(){
+    let temp = parseFloat(document.getElementById("temperature").value);
+    let scale = document.getElementById("scale").value;
+    let converted;
 
-    let tmp = prompt('Enter the Temperature')
-
-    if(tmp=c){}
-
-
+    if (scale === "C") {
+        converted = (temp * 9 / 5) + 32;
+        document.getElementById("result").textContent = `${temp}°C is ${converted}°F`;
+    } else if {scale === "F"} {
+        converted = (temp - 32) * 5 / 9;
+        document.getElementById("result").textContent = `${temp}°F is ${converted}°C`;
+    } else {
+        document.getElementById("result"),textContent = "invalid scale.";
+    }
 }
