d982a30030aa2b906bcd5ee92d703842dcfed913
Mike Miller <millms302@gmail.com>
2025-10-06 18:27:11 -0500

    Assignment3finalv2

--- scripts/assignment3.js
+++ scripts/assignment3.js
@@ -1,15 +1,16 @@
-function convertTemperature(){
-    let temp = parseFloat(document.getElementById("temperature").value);
-    let scale = document.getElementById("scale").value;
-    let converted;
 
-    if (scale === "C") {
-        converted = (temp * 9 / 5) + 32;
-        document.getElementById("result").textContent = `${temp}°C is ${converted}°F`;
-    } else if {scale === "F"} {
-        converted = (temp - 32) * 5 / 9;
-        document.getElementById("result").textContent = `${temp}°F is ${converted}°C`;
-    } else {
-        document.getElementById("result"),textContent = "invalid scale.";
-    }
-}
+    function convertTemperature() {
+            let temp = parseFloat(document.getElementById("temperature").value);
+            let scale = document.getElementById("scale").value;
+            let converted;
+
+            if (scale === "C") {
+              converted = (temp * 9 / 5) + 32;
+              document.getElementById("result").textContent = `${temp}°C is ${converted}°F`;
+            } else if (scale === "F") {
+              converted = (temp - 32) * 5 / 9;
+              document.getElementById("result").textContent = `${temp}°F is ${converted}°C`;
+            } else {
+              document.getElementById("result"),textContent = "invalid scale.";
+            }
+        }
